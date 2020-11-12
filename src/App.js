9b31ff0e6cabad4a2441b9a047cd3161d6108510
Arthur Abreu Damiani <arthur.damiani@gmail.com>
2020-11-12 17:47:09 -0300

    footer inicialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Routes from './routes';
 import './styles/global.css';
 
 function App() {
   return (
-    <div >
-      <h1>oi</h1>
-    </div>
+    <Routes />
   );
 }
 
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+import './style.css';
+
+function Footer() {
+    return(
+        <footer id="rodape">
+            <div className="lado-direitro-rodape">
+                <a className="logo-rodape" href="#cabecalho">DevPlans</a>
+                <div className="endereco">
+                    <p className="endereco-email">contato@devplans.com.br</p>
+                    <div className="endereco-fisico">
+                        <div className="endereco-fisico-comeco">
+                            <p>Rodovia Aparício Ramos Cordeiro,</p>
+                            <p>180</p>
+                        </div>
+                        <p>Casa 18 - CEP 88065-500</p>
+                    </div>
+                </div>
+            </div>
+            <div className="lado-esquerdo-rodape">
+                <div>
+                    <ul className="lista">
+                        <li className="lista-item"><a href="#"><FontAwesomeIcon icon={faLinkedinIn}/></a></li>
+                        <li className="lista-item item-meio"><a href="#"><FontAwesomeIcon icon={faFacebookF}/></a></li>
+                        <li className="lista-item"><a href="#"><FontAwesomeIcon icon={faInstagram}/></a></li>
+                    </ul>
+                </div>
+                <p>&copy; 2020 Devplans.</p>
+            </div>
+	    </footer>
+    )
+}
+
+export default Footer;
